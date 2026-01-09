26286253424b45a06ab04ea15ee77de5b7a53850
martimrteixeira <martim.r.teixeira@inesctec.pt>
2026-01-09 16:21:17 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "technova-v1";
+const CACHE_NAME = "technova-v2";
 
 const FILES_TO_CACHE = [
   "/",
@@ -21,3 +21,4 @@ self.addEventListener("fetch", event => {
     })
   );
 });
+
